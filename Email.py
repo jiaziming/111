7e45cc505d7731367fe430ed2a657c3fc0f03074
jiaziming <jiaziming123@sina.cn>
2024-05-10 18:15:22 +0800

    test123

--- Email.py
+++ Email.py
@@ -2,7 +2,7 @@
 
 
 #import smtplib
-from email.mime.multipart import MIMEMultipart
+#from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.header import Header
 
